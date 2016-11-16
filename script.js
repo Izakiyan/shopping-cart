d95206bb1ad37c42187c94568c41730a1bf7f792
Yaniv Izaki <Yaniv Izaki>
2016-11-16 16:52:39 +0200

    .

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 // an array with all of our cart items
 var cart = [];
 
+var CartItem = function (name, price) {
+	this.name = name;
+	this.price = price;
+}
+
 var updateCart = function () {
-  // TODO: finish
+  for (var i = 0; i < cart.length; i++) {
+  	cart[i]
+  }
 }
 
 
 var addItem = function (item) {
-  // TODO: finish
+  cart.push(item);
 }
 
 var clearCart = function () {
-  // TODO: finish
+  
 }
 
 $('.view-cart').on('click', function () {
   // TODO: hide/show the shopping cart!
+  $('.shopping-cart').toggle();
 });
 
 $('.add-to-cart').on('click', function () {
   // TODO: get the "item" object from the page
+  var datt = $(this).closest('div[item]').data();
+  var item = new CartItem(datt.name,datt.price);
   addItem(item);
   updateCart();
 });
